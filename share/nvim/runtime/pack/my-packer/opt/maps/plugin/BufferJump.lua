98b051c62446d84cd76b3bba9e15df0cf19a684d
peter-lyr <llydrp.ldp@gmail.com>
2023-03-07 22:34:23 +0800

    leader leader w

--- share/nvim/runtime/pack/my-packer/opt/maps/plugin/BufferJump.lua
+++ share/nvim/runtime/pack/my-packer/opt/maps/plugin/BufferJump.lua
@@ -5,8 +5,8 @@ s({'n', 'v'}, '<leader>s', '<cmd>:call BufferJump#Down()<cr>', {silent = true})
 s({'n', 'v'}, '<leader>a', '<cmd>:call BufferJump#Left()<cr>', {silent = true})
 s({'n', 'v'}, '<leader>d', '<cmd>:call BufferJump#Right()<cr>', {silent = true})
 
-s({'n', 'v'}, '<leader>W', '<cmd>:call BufferJump#Miximize(1)<cr>', {silent = true})
-s({'n', 'v'}, '<leader>S', '<cmd>:call BufferJump#Miximize(0)<cr>', {silent = true})
+s({'n', 'v'}, '<leader><leader>w', '<cmd>:call BufferJump#Miximize(1)<cr>', {silent = true})
+s({'n', 'v'}, '<leader><leader>s', '<cmd>:call BufferJump#Miximize(0)<cr>', {silent = true})
 
 s({'n', 'v'}, '<leader>o', '<cmd>:call BufferJump#MaxHeight()<cr>', {silent = true})
 s({'n', 'v'}, '<leader>i', '<cmd>:call BufferJump#SameWidthHeight()<cr>', {silent = true})
