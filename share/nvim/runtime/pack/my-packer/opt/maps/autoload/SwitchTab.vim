324e9703a794e6ca86cdb88af96f2f58f68025b0
peter-lyr <llydrp.ldp@gmail.com>
2023-03-08 21:02:47 +0800

    deleted SwitchTab.vim

--- share/nvim/runtime/pack/my-packer/opt/maps/autoload/SwitchTab.vim
+++
@@ -1,7 +0,0 @@
-fu! SwitchTab#SpaceCr()
-  if !exists('g:lasttab')
-    echomsg '2222222'
-    return
-  endif
-  exe "tabn ".g:lasttab
-endfu
--- share/nvim/runtime/pack/my-packer/opt/maps/plugin/SwitchTab.lua
+++ share/nvim/runtime/pack/my-packer/opt/maps/plugin/SwitchTab.lua
@@ -10,7 +10,14 @@ s({ 'n', 'v', }, '<leader>7', '7gt', {silent = true})
 s({ 'n', 'v', }, '<leader>8', '8gt', {silent = true})
 s({ 'n', 'v', }, '<leader>9', '9gt', {silent = true})
 s({ 'n', 'v', }, '<leader>0', '<cmd>:tablast<cr>', {silent = true})
-s({ 'n', 'v', }, '<leader><cr>', '<cmd>:call SwitchTab#SpaceCr()<cr>', {silent = true})
+
+local space_enter = function()
+  if vim.fn['exists']('g:lasttab') == 1 then
+    vim.cmd("tabn " .. vim.g.lasttab)
+  end
+end
+
+s({ 'n', 'v', }, '<leader><cr>', space_enter, {silent = true})
 
 s({'n', 'v'}, '<cr>', '<cmd>:tabnext<cr>', {silent = true})
 s({'n', 'v'}, '<s-cr>', '<cmd>:tabprevious<cr>', {silent = true})
@@ -23,7 +30,6 @@ s({ 'n', 'v', }, '<c-s-j>', 'gt', {silent = true})
 local tab_leave = function()
   vim.g.lasttab = vim.fn['tabpagenr']()
 end
-
 vim.api.nvim_create_autocmd({"TabLeave"}, {
   callback = tab_leave,
 })
