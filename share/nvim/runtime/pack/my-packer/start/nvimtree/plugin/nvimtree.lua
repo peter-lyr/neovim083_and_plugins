bdd9027944f9bb497496a58ab6f01f81170e8334
peter-lyr <llydrp.ldp@gmail.com>
2023-03-14 14:25:51 +0800

    不用nvimtree了，跟netrw有冲突

--- share/nvim/runtime/pack/my-packer/start/nvimtree/plugin/nvimtree.lua
+++ share/nvim/runtime/pack/my-packer/start/nvimtree/plugin/nvimtree.lua
@@ -1,25 +1,25 @@
-local nvimtree_exe = function(cmd)
-  if not vim.g.loaded_config_nvimtree then
-    vim.g.loaded_config_nvimtree = 1
-    local sta, config_nvimtree = pcall(require, 'config_nvimtree')
-    if not sta then
-      print('no config_nvimtree')
-      return
-    end
-  end
-  local sta, toggle_nvimtree = pcall(require, 'toggle_nvimtree')
-  if not sta then
-    print('no toggle_nvimtree')
-    return
-  end
-  if cmd == 'toggle_search_fname' then
-    toggle_nvimtree.toggle('cur_fname')
-  elseif cmd == 'toggle_search_cwd' then
-    toggle_nvimtree.toggle('cwd')
-  end
-end
-
-local s = vim.keymap.set
-
-s({'n', 'v'}, '<leader><leader>;', function() nvimtree_exe("toggle_search_fname") end, {silent = true})
-s({'n', 'v'}, '<leader><leader>\'', function() nvimtree_exe("toggle_search_cwd") end, {silent = true})
+-- local nvimtree_exe = function(cmd)
+--   if not vim.g.loaded_config_nvimtree then
+--     vim.g.loaded_config_nvimtree = 1
+--     local sta, config_nvimtree = pcall(require, 'config_nvimtree')
+--     if not sta then
+--       print('no config_nvimtree')
+--       return
+--     end
+--   end
+--   local sta, toggle_nvimtree = pcall(require, 'toggle_nvimtree')
+--   if not sta then
+--     print('no toggle_nvimtree')
+--     return
+--   end
+--   if cmd == 'toggle_search_fname' then
+--     toggle_nvimtree.toggle('cur_fname')
+--   elseif cmd == 'toggle_search_cwd' then
+--     toggle_nvimtree.toggle('cwd')
+--   end
+-- end
+--
+-- local s = vim.keymap.set
+--
+-- s({'n', 'v'}, '<leader><leader>;', function() nvimtree_exe("toggle_search_fname") end, {silent = true})
+-- s({'n', 'v'}, '<leader><leader>\'', function() nvimtree_exe("toggle_search_cwd") end, {silent = true})
