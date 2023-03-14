c29c3db5117fccd5df3c87ba305fafdb1cba38b7
peter-lyr <llydrp.ldp@gmail.com>
2023-03-14 13:42:14 +0800

    auto-save-session.lua

--- share/nvim/runtime/pack/my-packer/start/plugins/plugin/auto-save-session.lua
+++ share/nvim/runtime/pack/my-packer/start/plugins/plugin/auto-save-session.lua
@@ -2,6 +2,6 @@ local g = vim.g
 
 g.auto_save = 1
 g.auto_save_silent = 1
-g.auto_save_events = ['InsertLeave', 'TextChanged', 'TextChangedI', 'CursorHold', 'CursorHoldI', 'CompleteDone']
+g.auto_save_events = {'InsertLeave', 'TextChanged', 'TextChangedI', 'CursorHold', 'CursorHoldI', 'CompleteDone'}
 g.session_autoload = 'no'
 g.session_autosave = 'yes'
