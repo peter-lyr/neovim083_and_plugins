cff7f13544d0b6cedc699b42b9994af7e8d6f9f2
peter-lyr <llydrp.ldp@gmail.com>
2023-03-16 01:19:47 +0800

    gitpush cd wrong

--- share/nvim/runtime/pack/my-packer/start/gitpush/lua/gitpush.lua
+++ share/nvim/runtime/pack/my-packer/start/gitpush/lua/gitpush.lua
@@ -2,6 +2,7 @@ local M = {}
 
 local g = vim.g
 local f = vim.fn
+local a = vim.api
 
 local Path = require "plenary.path"
 local path = Path:new(g.gitpush_lua)
@@ -10,7 +11,7 @@ g.gitpush_dir = path:parent():parent()['filename']
 
 function M.add_commit_push()
   local bat = Path:new(g.gitpush_dir):joinpath('autoload', 'add_commit_push.bat')['filename']
-  f['system'](string.format('cd %s && start cmd /c "%s"', g.gitpush_dir, bat))
+  f['system'](string.format('cd %s && start cmd /c "%s"', Path:new(a['nvim_buf_get_name'](0)):parent()['filename'], bat))
 end
 
 return M
