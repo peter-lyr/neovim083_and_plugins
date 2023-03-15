86abbb81b5ca480a0385ca28bf6ec918e803c3d7
peter-lyr <llydrp.ldp@gmail.com>
2023-03-15 11:59:02 +0800

    colorscheme cursormoved

--- share/nvim/runtime/pack/my-packer/start/autopairs/plugin/autopairs.lua
+++ share/nvim/runtime/pack/my-packer/start/autopairs/plugin/autopairs.lua
@@ -1,14 +1,17 @@
 local g = vim.g
 local a = vim.api
 
-g.autopairs_insertenter = a.nvim_create_autocmd({"InsertEnter"}, {
-  callback = function()
-    a.nvim_del_autocmd(g.autopairs_insertenter)
-    local sta, autopairs = pcall(require, "nvim-autopairs")
-    if not sta then
-      print("no nvim-autopairs")
-      return
-    end
-    autopairs.setup()
-  end,
-})
+if not g.autopairs_loaded then
+  g.autopairs_loaded = 1
+  g.autopairs_insertenter = a.nvim_create_autocmd({"InsertEnter"}, {
+    callback = function()
+      a.nvim_del_autocmd(g.autopairs_insertenter)
+      local sta, autopairs = pcall(require, "nvim-autopairs")
+      if not sta then
+        print("no nvim-autopairs")
+        return
+      end
+      autopairs.setup()
+    end,
+  })
+end
--- share/nvim/runtime/pack/my-packer/start/plugins/plugin/colorscheme.lua
+++ share/nvim/runtime/pack/my-packer/start/plugins/plugin/colorscheme.lua
@@ -1,4 +1,17 @@
-local sta, _ = pcall(vim.cmd, 'colorscheme sierra')
-if not sta then
-  return
+local g = vim.g
+local a = vim.api
+local c = vim.cmd
+
+if not g.colorscheme_loaded then
+  g.colorscheme_loaded = 1
+  g.colorscheme_cursormoved = a.nvim_create_autocmd({"CursorMoved"}, {
+    callback = function()
+      a.nvim_del_autocmd(g.colorscheme_cursormoved)
+      local sta, _ = pcall(c, 'colorscheme sierra')
+      if not sta then
+        print("no colorscheme sierra")
+        return
+      end
+    end,
+  })
 end
