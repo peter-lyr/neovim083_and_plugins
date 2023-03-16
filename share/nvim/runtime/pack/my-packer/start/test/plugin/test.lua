64b9e8112849a25b63373b71654199fc0eea2224
peter-lyr <llydrp.ldp@gmail.com>
2023-03-16 19:35:15 +0800

    test terminal

---
+++ share/nvim/runtime/pack/my-packer/start/test/plugin/test.lua
@@ -0,0 +1,112 @@
+local f = vim.fn
+local c = vim.cmd
+local o = vim.opt
+local a = vim.api
+local m = string.match
+local s = vim.keymap.set
+
+local is_terminal = function(bufname, terminal)
+  if m(bufname, '^term://') then
+    local ret = false
+    local is_ipython = m(bufname, ':ipython$')
+    local is_bash = m(bufname, ':bash$')
+    local is_powershell = m(bufname, ':powershell$')
+    if terminal == 'ipython' and is_ipython then
+      ret = true
+    elseif terminal == 'bash' and is_bash then
+      ret = true
+    elseif terminal == 'powershell' and is_powershell then
+      ret = true
+    end
+    if ret then
+      return true, true
+    elseif terminal == '' and not is_powershell and not is_bash and not is_ipython then
+      return true, true
+    end
+    return true, false
+  end
+  return false, false
+end
+
+local try_goto_terminal = function()
+  for i = 1, f['winnr']('$') do
+    local bufnr = f['winbufnr'](i)
+    local buftype = f['getbufvar'](bufnr, '&buftype')
+    if buftype == 'terminal' then
+      f['win_gotoid'](f['win_getid'](i))
+      return true
+    end
+  end
+  return false
+end
+
+local get_terminal_bufnrs = function(terminal)
+  local terminal_bufnrs = {}
+  for k, v in pairs(f['getbufinfo']()) do
+    local one, certain = is_terminal(v['name'], terminal)
+    if certain then
+      table.insert(terminal_bufnrs, v['bufnr'])
+    end
+  end
+  if #terminal_bufnrs == 0 then
+    return nil
+  end
+  return terminal_bufnrs
+end
+
+function index_of(arr, val)
+  if not arr then
+    return nil
+  end
+  for i, v in ipairs(arr) do
+    if v == val then
+      return i
+    end
+  end
+  return nil
+end
+
+local test = function(terminal)
+  print(terminal)
+  local fname = a['nvim_buf_get_name'](0)
+  local bnr = f['bufnr']()
+  local terminal_bufnrs = get_terminal_bufnrs(terminal)
+  local one, certain = is_terminal(fname, terminal)
+  if certain then
+    if #terminal_bufnrs == 1 then
+      if f['winnr']('$') > 1 then
+        c'hide'
+      end
+      return
+    end
+    bnr_idx = index_of(terminal_bufnrs, f['bufnr']())
+    bnr_idx = bnr_idx + 1
+    if bnr_idx > #terminal_bufnrs then
+      if f['winnr']('$') > 1 then
+        c'hide'
+      end
+      return
+    else
+      c(string.format("b%d", terminal_bufnrs[bnr_idx]))
+    end
+  else
+    if terminal_bufnrs then
+      if not try_goto_terminal() then
+        if #fname > 0 or o.modified:get() == true then
+          c'split'
+        end
+      end
+      c(string.format("b%d", terminal_bufnrs[1]))
+    else
+      if not one then
+        c'split'
+      end
+      c(string.format('te %s', terminal))
+    end
+  end
+end
+
+s('n', '\\q', function() test('') end, { silent = true})
+s('n', '\\w', function() test('ipython') end, { silent = true})
+s('n', '\\e', function() test('bash') end, { silent = true})
+s('n', '\\r', function() test('powershell') end, { silent = true})
