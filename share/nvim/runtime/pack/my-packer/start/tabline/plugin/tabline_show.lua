36134405b042d18e20aaaaf9bc7ccc0e3de2be86
peter-lyr <llydrp.ldp@gmail.com>
2023-03-18 16:27:23 +0800

    tabline titlestring

--- share/nvim/runtime/pack/my-packer/start/tabline/plugin/tabline_show.lua
+++ share/nvim/runtime/pack/my-packer/start/tabline/plugin/tabline_show.lua
@@ -1,12 +1,47 @@
 local g = vim.g
+local o = vim.opt
 local c = vim.cmd
 local a = vim.api
+local f = vim.fn
 
-local tab_enter = function()
-  a.nvim_del_autocmd(g.set_tabline)
-  c([[set tabline=%!tabline#tabline()]])
+local get_fname_tail = function(fname)
+  local fname = string.gsub(fname, "\\", '/')
+  local sta, path = pcall(require, "plenary.path")
+  if not sta then
+    print('tabline_show no plenary.path')
+    return ''
+  end
+  local path = path:new(fname)
+  if path:is_file() then
+    local fname = path:_split()
+    return fname[#fname]
+  elseif path:is_dir() then
+    local fname = path:_split()
+    if #fname[#fname] > 0 then
+      return fname[#fname]
+    else
+      return fname[#fname-1]
+    end
+  end
+  return ''
 end
 
 g.set_tabline = a.nvim_create_autocmd({"TabEnter"}, {
-  callback = tab_enter,
+  callback = function()
+    a.nvim_del_autocmd(g.set_tabline)
+    c([[set tabline=%!tabline#tabline()]])
+  end,
+})
+
+a.nvim_create_autocmd({"WinLeave"}, {
+  callback = function()
+    local timer = vim.loop.new_timer()
+    timer:start(100, 750, vim.schedule_wrap(function()
+      timer:close()
+      local title = get_fname_tail(f['getcwd']())
+      if #title > 0 then
+        o.titlestring = title
+      end
+    end))
+  end,
 })
