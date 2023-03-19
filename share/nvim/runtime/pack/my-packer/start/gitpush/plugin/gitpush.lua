71818748c3870a482697d7f7e7c80dde7f1478ea
peter-lyr <llydrp.ldp@gmail.com>
2023-03-19 22:15:59 +0800

    优化gitpush

--- share/nvim/runtime/pack/my-packer/start/gitpush/plugin/gitpush.lua
+++ share/nvim/runtime/pack/my-packer/start/gitpush/plugin/gitpush.lua
@@ -1,18 +1,19 @@
 local g = vim.g
 local s = vim.keymap.set
 
-
 g.gitpush_lua = vim.fn['expand']('<sfile>')
 
-
 local gitpush_exe = function(cmd)
-  local sta, gitpush = pcall(require, 'gitpush')
-  if not sta then
-    print('no gitpush')
-    return
+  if not g.loaded_do_gitpush then
+    g.loaded_do_gitpush = 1
+    sta, do_gitpush = pcall(require, 'do_gitpush')
+    if not sta then
+      print('no do_gitpush')
+      return
+    end
   end
   if cmd == 'add_commit_push' then
-    gitpush.add_commit_push()
+    do_gitpush.add_commit_push()
   end
 end
 
