af509088420f16f40e228f7bf0c3cedbd9682195
peter-lyr <llydrp.ldp@gmail.com>
2023-03-22 12:05:48 +0800

    bufferenter netrw winfixwidth

--- share/nvim/runtime/pack/my-packer/start/bufferenter/plugin/bufferenter.lua
+++ share/nvim/runtime/pack/my-packer/start/bufferenter/plugin/bufferenter.lua
@@ -1,3 +1,4 @@
+local g = vim.g
 local a = vim.api
 local o = vim.opt
 local s = vim.keymap.set
@@ -5,6 +6,21 @@ local s = vim.keymap.set
 local bufenter = function()
   if o.ft:get() == 'lua' then
     s({'n', 'v'}, 'K', '<nop>', {buffer = true})
+  elseif o.ft:get() == 'netrw' then
+    if o.winfixwidth:get() then
+      if not g.bufferenter_do_netrw then
+        g.bufferenter_do_netrw = 1
+        local sta, toggle_netrw = pcall(require, 'toggle_netrw')
+        if not sta then
+          print('no toggle_netrw')
+          return
+        end
+      end
+      if not toggle_netrw then
+        return
+      end
+      toggle_netrw.netrw_fix_set_width()
+    end
   end
 end
 
