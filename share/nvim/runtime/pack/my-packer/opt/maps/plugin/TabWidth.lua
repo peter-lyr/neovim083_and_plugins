44d7a2eb7e3bf28c2272737921135cff754d55fa
peter-lyr <llydrp.ldp@gmail.com>
2023-03-31 22:40:48 +0800

    function isInTable(value, tbl)

--- share/nvim/runtime/pack/my-packer/opt/maps/plugin/TabWidth.lua
+++ share/nvim/runtime/pack/my-packer/opt/maps/plugin/TabWidth.lua
@@ -1,8 +1,23 @@
 local o = vim.opt
 local a = vim.api
 
+function isInTable(value, tbl)
+  for k,v in ipairs(tbl) do
+    if v == value then
+      return true
+    end
+  end
+  return false
+end
+
+local lang = {
+  'c',
+  'cpp',
+  'python',
+}
+
 local tab_width = function()
-  if o.filetype:get() == 'c' or o.filetype:get() == 'cpp' then
+  if isInTable(o.filetype:get(), lang) then
     o.tabstop = 4
     o.softtabstop = 4
     o.shiftwidth = 4
