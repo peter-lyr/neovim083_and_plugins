6503ca1a642b165b0bd9ef9e1af8eb002f298ca8
peter-lyr <llydrp.ldp@gmail.com>
2023-04-01 04:33:09 +0800

    testdragfile.c判断是否拖拽的csv文件

--- share/nvim/runtime/pack/my-packer/start/c/autoload/testdragfile/testdragfile.c
+++ share/nvim/runtime/pack/my-packer/start/c/autoload/testdragfile/testdragfile.c
@@ -4,14 +4,16 @@
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
+        printf("Usage: %s filename.csv\n", argv[0]);
         system("pause");
         return 1;
     }
 
-    for (int i=1; i<argc; i++)
-    {
-        printf("%s\n", argv[i]);
+    char *extension = strrchr(argv[1], '.');
+    if (extension == NULL || strcmp(extension, ".csv") != 0) {
+        printf("Error: %s is not a csv file\n", argv[1]);
+        system("pause");
+        return 1;
     }
 
     system("pause");
