63079bd49599752a1b1342750406bf8b2f0c9887
peter-lyr <llydrp.ldp@gmail.com>
2023-04-03 15:04:49 +0800

    scan_dir

--- share/nvim/runtime/pack/my-packer/start/c/autoload/testscanfiles/testscanfiles.c
+++ share/nvim/runtime/pack/my-packer/start/c/autoload/testscanfiles/testscanfiles.c
@@ -1,41 +1,44 @@
+// C语言定义一个函数：共3个参数，第一个参数是需要扫描的目录，第二个是保存扫描到的文件名组成的数组指针，第三个是数组长度指针，功能是扫描一个文件夹下的所有文件，放到一个数组里，第二个参数指向它，第三个参数指向它的长度。
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 
-int scan_files(const char *scan_dir)
+void scan_dir(const char *dir_name, char ***files, int *n)
 {
     DIR *dir;
     struct dirent *ent;
-    char **Files = NULL;
-    int n = 0;
 
-    if ((dir = opendir (scan_dir)) != NULL) {
-        while ((ent = readdir (dir)) != NULL) {
+    if ((dir = opendir(dir_name)) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
             if (ent->d_type == DT_REG) { /* If the entry is a regular file */
-                Files = realloc (Files, sizeof (char*) * ++n);
-                Files[n-1] = malloc (strlen(ent->d_name) + 1);
-                strcpy (Files[n-1], ent->d_name);
+                *files = realloc(*files, sizeof(char*) * ++(*n));
+                (*files)[*n-1] = malloc(strlen(ent->d_name) + 1);
+                strcpy((*files)[*n-1], ent->d_name);
             }
         }
-        closedir (dir);
+        closedir(dir);
     } else {
-        perror ("");
-        return EXIT_FAILURE;
+        perror("");
+        exit(EXIT_FAILURE);
     }
-
-    /* ... */
-
-    for (int i = 0; i < n; i++) {
-        printf ("%s\n", Files[i]);
-        free (Files[i]);
-    }
-    free (Files);
-
-    return EXIT_SUCCESS;
 }
 
 int main()
 {
-    scan_files(".");
+    char **Files = NULL;
+    int n = 0;
+
+    scan_dir(".", &Files, &n);
+
+    /* ... */
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", Files[i]);
+        free(Files[i]);
+    }
+    free(Files);
+
+    return EXIT_SUCCESS;
 }
