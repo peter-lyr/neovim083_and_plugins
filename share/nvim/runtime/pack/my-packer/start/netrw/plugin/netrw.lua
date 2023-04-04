5be2fc2b7a22cbb9f7c7170b5606c467aaf5e5de
peter-lyr <llydrp.ldp@gmail.com>
2023-04-05 03:04:22 +0800

    g.netrw_hide = 0

--- share/nvim/runtime/pack/my-packer/start/netrw/plugin/netrw.lua
+++ share/nvim/runtime/pack/my-packer/start/netrw/plugin/netrw.lua
@@ -43,6 +43,7 @@ g.netrw_alto = 0
 g.netrw_winsize = 120
 g.netrw_list_hide = ""
 g.netrw_dirhistmax = 0
+g.netrw_hide = 0
 
 s({'n', 'v'}, '<leader>l', function() netrw_exe("toggle_fix") end, {silent = true})
 s({'n', 'v'}, '<leader><leader>l', function() netrw_exe("fix_unfix") end, {silent = true})
