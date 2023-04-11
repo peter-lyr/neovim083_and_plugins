cc40d56a24e556c86f44df45b0e4c76daad96f3b
peter-lyr <llydrp.ldp@gmail.com>
2023-04-11 16:39:52 +0800

    do_sdkcbp test

--- share/nvim/runtime/pack/my-packer/start/sdkcbp/lua/do_sdkcbp.lua
+++ share/nvim/runtime/pack/my-packer/start/sdkcbp/lua/do_sdkcbp.lua
@@ -29,53 +29,53 @@ function show_array(arr)
   end
 end
 
-function search_cbp(dirPath)
-  cnt = 0
-  to_break = 0
-  for i, j, k in os.walk(dirPath) do
-    if to_break == 1 then
-      break
-    end
-    for f in k do
-      cnt += 1
-      if cnt >= 1000 then
-        to_break = 1
-        break
-      end
-      if f.split('.')[-1] == 'cbp' then
-        vim.command(f"let cbps += ['{os.path.join(i, f)}']")
-      end
-    end
-  end
-  return cbps
-end
+-- function search_cbp(dirPath)
+--   cnt = 0
+--   to_break = 0
+--   for i, j, k in os.walk(dirPath) do
+--     if to_break == 1 then
+--       break
+--     end
+--     for f in k do
+--       cnt += 1
+--       if cnt >= 1000 then
+--         to_break = 1
+--         break
+--       end
+--       if f.split('.')[-1] == 'cbp' then
+--         vim.command(f"let cbps += ['{os.path.join(i, f)}']")
+--       end
+--     end
+--   end
+--   return cbps
+-- end
 
 function find_cbp()
-  local cbpPath = f['expand']('%:p')
-  if split(cbpPath, '\\.')[-1] == 'cbp' then
-    return {cbpPath}
+  local fname = a['nvim_buf_get_name'](0)
+  local cbp_path = Path:new(fname)
+  if string.match(fname, '%.(%w+)$') == 'cbp' then
+    return {fname}
   end
-  local dirPath = a['nvim_buf_get_name'](0)
-  while string.gmatch(dirPath, '\\') do
-    local dirPath = parentDir(dirPath)
-    local subDir = printf('%s\%s', dirPath, 'app')
-    if isdirectory(subDir) then
-      return search_cbp(subDir)
-    end
-    local dirName = split(dirPath, '\\')[-1]
-    if 'app' == dirName then
-      return search_cbp(dirPath)
-    end
-    if 'libs' == dirName then
-      return search_cbp(ParentDir(dirPath) .. '\\app')
-    end
-  end
+  -- local dirPath = a['nvim_buf_get_name'](0)
+  -- while string.gmatch(dirPath, '\\') do
+  --   local dirPath = parentDir(dirPath)
+  --   local subDir = printf('%s\%s', dirPath, 'app')
+  --   if isdirectory(subDir) then
+  --     return search_cbp(subDir)
+  --   end
+  --   local dirName = split(dirPath, '\\')[-1]
+  --   if 'app' == dirName then
+  --     return search_cbp(dirPath)
+  --   end
+  --   if 'libs' == dirName then
+  --     return search_cbp(ParentDir(dirPath) .. '\\app')
+  --   end
+  -- end
   return {}
 end
 
 function M.do_sdkcbp(cmd)
+  show_array(find_cbp())
 end
-
-show_array(find_cbp())
 
 return M
