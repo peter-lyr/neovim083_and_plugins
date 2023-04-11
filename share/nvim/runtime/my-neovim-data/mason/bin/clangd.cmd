142e50f51de235f77e25505b6d503b94eb99ef72
peter-lyr <llydrp.ldp@gmail.com>
2023-04-11 16:40:02 +0800

    mason cmd

--- share/nvim/runtime/my-neovim-data/mason/bin/clangd.cmd
+++ share/nvim/runtime/my-neovim-data/mason/bin/clangd.cmd
@@ -7,5 +7,5 @@ EXIT /b
 SETLOCAL
 CALL :find_dp0
 
-REM  endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "D:\Desktop\neovim083_and_plugins\share\nvim\runtime\my-neovim-data\mason\packages\clangd\clangd\bin\clangd.exe" %*
-endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "C:\Users\llydr\Desktop\neovim083_and_plugins\share\nvim\runtime\my-neovim-data\mason\packages\clangd\clangd\bin\clangd.exe" %*
+endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "D:\Desktop\neovim083_and_plugins\share\nvim\runtime\my-neovim-data\mason\packages\clangd\clangd\bin\clangd.exe" %*
+REM  endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "C:\Users\llydr\Desktop\neovim083_and_plugins\share\nvim\runtime\my-neovim-data\mason\packages\clangd\clangd\bin\clangd.exe" %*
