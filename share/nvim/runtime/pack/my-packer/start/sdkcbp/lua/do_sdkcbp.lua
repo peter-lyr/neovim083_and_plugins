d62bc556f7e76bf5fae2bc760ee61e5042930cf8
peter-lyr <llydrp.ldp@gmail.com>
2023-04-11 20:27:01 +0800

    sdkcbp test scan dir up

--- share/nvim/runtime/pack/my-packer/start/sdkcbp/lua/do_sdkcbp.lua
+++ share/nvim/runtime/pack/my-packer/start/sdkcbp/lua/do_sdkcbp.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local Path = require("plenary.path")
+local Scan = require("plenary.scandir")
 
 local g = vim.g
 local b = vim.b
@@ -25,7 +26,7 @@ end
 
 function show_array(arr)
   for i, v in ipairs(arr) do
-    print(i, ':', v)
+    -- print(i, ':', v)
   end
 end
 
@@ -50,32 +51,70 @@ end
 --   return cbps
 -- end
 
-function find_cbp()
-  local fname = a['nvim_buf_get_name'](0)
-  local cbp_path = Path:new(fname)
-  if string.match(fname, '%.(%w+)$') == 'cbp' then
-    return {fname}
+M.searched_folders = {}
+
+function M.traverse_folder(abspath)
+  local path = Path:new(abspath)
+  local entries = Scan.scan_dir(path.filename, { hidden = 1, depth = 1, add_dirs = true })
+  for _, entry in ipairs(entries) do
+    local entry_path = Path:new(entry)
+    local entry_path_name = entry_path.filename
+    if entry_path:is_file() then
+      if string.match(entry_path_name, '%.([^%.]+)$') == 'cbp' then
+        -- print(entry_path_name)
+        break
+      end
+    else
+      if not index_of(M.searched_folders, entry_path_name) then
+        print(entry_path_name)
+        table.insert(M.searched_folders, entry_path_name)
+        M.traverse_folder(entry_path_name)
+      end
+    end
   end
-  -- local dirPath = a['nvim_buf_get_name'](0)
-  -- while string.gmatch(dirPath, '\\') do
-  --   local dirPath = parentDir(dirPath)
-  --   local subDir = printf('%s\%s', dirPath, 'app')
-  --   if isdirectory(subDir) then
-  --     return search_cbp(subDir)
-  --   end
-  --   local dirName = split(dirPath, '\\')[-1]
-  --   if 'app' == dirName then
-  --     return search_cbp(dirPath)
-  --   end
-  --   if 'libs' == dirName then
-  --     return search_cbp(ParentDir(dirPath) .. '\\app')
-  --   end
-  -- end
+end
+
+function M.find_cbp()
+  local fname = a['nvim_buf_get_name'](0)
+  local path = Path:new(fname)
+  if not path:exists() then
+    return {}
+  end
+  if path:is_file() then
+    if string.match(fname, '%.([^%.]+)$') == 'cbp' then
+      return {fname}
+    else
+      dname = path:parent()
+    end
+    dname = path
+    M.searched_folders = {}
+    local cnt = 0
+    while 1 do
+      M.traverse_folder(dname['filename'])
+      dname = dname:parent()
+      cnt = cnt + 1
+      if cnt > 20 then
+        break
+      end
+    end
+  --   -- local fname = parentDir(fname)
+  --   -- local subDir = printf('%s\%s', fname, 'app')
+  --   -- if isdirectory(subDir) then
+  --   --   return search_cbp(subDir)
+  --   -- end
+  --   -- local dirName = split(fname, '\\')[-1]
+  --   -- if 'app' == dirName then
+  --   --   return search_cbp(fname)
+  --   -- end
+  --   -- if 'libs' == dirName then
+  --   --   return search_cbp(ParentDir(fname) .. '\\app')
+  --   -- end
+  end
   return {}
 end
 
 function M.do_sdkcbp(cmd)
-  show_array(find_cbp())
+  M.find_cbp()
 end
 
 return M
