00f9a926b196bee20775b158b327328cb577a2bf
peter-lyr <llydrp.ldp@gmail.com>
2023-04-12 23:20:19 +0800

    GetClipboardImage.ps1

--- share/nvim/runtime/pack/my-packer/start/markdownimage/autoload/GetClipboardImage.ps1
+++ share/nvim/runtime/pack/my-packer/start/markdownimage/autoload/GetClipboardImage.ps1
@@ -4,9 +4,9 @@ Add-Type -AssemblyName System.Windows.Forms;
 if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {
   $imageObj = [System.Drawing.Bitmap][System.Windows.Forms.Clipboard]::GetDataObject().getimage();
   if ($sel_jpg) {
-    $imageObj.Save($image_path + '.jpg', [System.Drawing.Imaging.ImageFormat]::Jpeg);
+    $imageObj.Save($image_path, [System.Drawing.Imaging.ImageFormat]::Jpeg);
   } else {
-    $imageObj.Save($image_path + '.png', [System.Drawing.Imaging.ImageFormat]::Png);
+    $imageObj.Save($image_path, [System.Drawing.Imaging.ImageFormat]::Png);
   }
   "success" | Out-File -FilePath $pipe_txt -NoNewLine -Encoding UTF8
 } else {
