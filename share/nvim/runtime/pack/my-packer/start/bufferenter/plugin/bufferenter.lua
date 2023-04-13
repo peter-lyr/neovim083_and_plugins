ffa31cd1d3fc790b0205f4d2789c873f3a47571b
peter-lyr <llydrp.ldp@gmail.com>
2023-04-13 22:45:23 +0800

    bufferenter.lua K nop cancel

--- share/nvim/runtime/pack/my-packer/start/bufferenter/plugin/bufferenter.lua
+++ share/nvim/runtime/pack/my-packer/start/bufferenter/plugin/bufferenter.lua
@@ -4,9 +4,9 @@ local o = vim.opt
 local s = vim.keymap.set
 
 local bufenter = function()
-  if o.ft:get() == 'lua' then
-    s({'n', 'v'}, 'K', '<nop>', {buffer = true})
-  end
+  -- if o.ft:get() == 'lua' then
+  --   s({'n', 'v'}, 'K', '<nop>', {buffer = true})
+  -- end
 end
 
 a.nvim_create_autocmd({"BufEnter"}, {
