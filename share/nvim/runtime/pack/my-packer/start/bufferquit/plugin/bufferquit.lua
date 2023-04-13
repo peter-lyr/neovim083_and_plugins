2c6d945e60c1e06ba5f3d33a04b09ec4c1a8e01d
peter-lyr <llydrp.ldp@gmail.com>
2023-04-13 23:09:52 +0800

    bufferquit.lua map leader x quit buffer

--- share/nvim/runtime/pack/my-packer/start/bufferquit/plugin/bufferquit.lua
+++ share/nvim/runtime/pack/my-packer/start/bufferquit/plugin/bufferquit.lua
@@ -1,5 +1,5 @@
 local s = vim.keymap.set
 
-s({'n', 'v'}, '<leader><leader>x', '<cmd>:try|hide|catch|endtry<cr>', {silent = true})
+s({'n', 'v'}, '<leader>x', '<cmd>:try|hide|catch|endtry<cr>', {silent = true})
 s({'n', 'v'}, '<a-bs>', '<cmd>:bw!<cr>', {silent = true})
 s({'n', 'v'}, 'ZX', '<cmd>:qa!<cr>', {silent = true})
