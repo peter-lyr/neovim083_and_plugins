6c9658f52e79547f41a580feb0ffc2e29202857a
peter-lyr <llydrp.ldp@gmail.com>
2023-04-13 22:33:36 +0800

    telescope.lua map

--- share/nvim/runtime/pack/my-packer/start/telescope/plugin/telescope.lua
+++ share/nvim/runtime/pack/my-packer/start/telescope/plugin/telescope.lua
@@ -35,23 +35,28 @@ local telescope_exe = function(cmd)
 end
 
 
-s({'n', 'v'}, '<leader><leader>fa', function() telescope_exe("builtin") end, {silent = true})
-s({'n', 'v'}, '<leader>fo', function() telescope_exe("oldfiles") end, {silent = true})
-s({'n', 'v'}, '<leader><leader>fh', function() telescope_exe("help_tags") end, {silent = true})
-s({'n', 'v'}, '<leader><leader>fl', function() telescope_exe("colorscheme") end, {silent = true})
-s({'n', 'v'}, '<leader>fc', function() telescope_exe("command_history") end, {silent = true})
-s({'n', 'v'}, '<leader><leader>fc', function() telescope_exe("commands") end, {silent = true})
-s({'n', 'v'}, '<leader>gf', function() telescope_exe("git_files") end, {silent = true})
-s({'n', 'v'}, '<leader>gc', function() telescope_exe("git_commits") end, {silent = true})
-s({'n', 'v'}, '<leader>gb', function() telescope_exe("git_bcommits") end, {silent = true})
-s({'n', 'v'}, '<leader>gh', function() telescope_exe("git_branches") end, {silent = true})
-s({'n', 'v'}, '<leader>gj', function() telescope_exe("git_status") end, {silent = true})
-s({'n', 'v'}, '<a-o>', function() telescope_exe("live_grep") end, {silent = true})
-s({'n', 'v'}, '<a-i>', function() telescope_exe("grep_string") end, {silent = true})
+s({'n', 'v'}, '<a-c>', function() telescope_exe("command_history") end, {silent = true})
+s({'n', 'v'}, '<a-C>', function() telescope_exe("commands") end, {silent = true})
+
+s({'n', 'v'}, '<a-o>', function() telescope_exe("oldfiles") end, {silent = true})
 s({'n', 'v'}, '<a-k>', function() telescope_exe("find_files") end, {silent = true})
-s({'n', 'v'}, '<a-b>', function() telescope_exe("buffers") end, {silent = true})
+s({'n', 'v'}, '<a-j>', function() telescope_exe("buffers") end, {silent = true})
+
+s({'n', 'v'}, '<a-;>k', function() telescope_exe("git_files") end, {silent = true})
+s({'n', 'v'}, '<a-;>i', function() telescope_exe("git_commits") end, {silent = true})
+s({'n', 'v'}, '<a-;>o', function() telescope_exe("git_bcommits") end, {silent = true})
+s({'n', 'v'}, '<a-;>h', function() telescope_exe("git_branches") end, {silent = true})
+s({'n', 'v'}, '<a-;>j', function() telescope_exe("git_status") end, {silent = true})
+
+s({'n', 'v'}, '<a-l>', function() telescope_exe("live_grep") end, {silent = true})
+s({'n', 'v'}, '<a-i>', function() telescope_exe("grep_string") end, {silent = true})
+
 s({'n', 'v'}, '<a-q>', function() telescope_exe("quickfix") end, {silent = true})
 s({'n', 'v'}, '<a-Q>', function() telescope_exe("quickfixhistory") end, {silent = true})
+
+s({'n', 'v'}, '<a-\'>a', function() telescope_exe("builtin") end, {silent = true})
+s({'n', 'v'}, '<a-\'>h', function() telescope_exe("help_tags") end, {silent = true})
+s({'n', 'v'}, '<a-\'>c', function() telescope_exe("colorscheme") end, {silent = true})
 
 
 s({'n', 'v'}, '<a-s-k>', function() telescope_exe("projects") end, {silent = true})
