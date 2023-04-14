c0195569290339408b33750ec18ed9697f05aa82
peter-lyr <llydrp.ldp@gmail.com>
2023-04-15 00:21:14 +0800

    fugitive

--- share/nvim/runtime/pack/my-packer/start/fugitive/plugin/fugitive.lua
+++ share/nvim/runtime/pack/my-packer/start/fugitive/plugin/fugitive.lua
@@ -1,4 +1,9 @@
-local fugitive_exe = function(cmd, refresh)
+local s = vim.keymap.set
+local a = vim.api
+local c = vim.cmd
+
+
+local fugitive_exe = function(cmd)
   if not vim.g.loaded_config_fugitive then
     vim.g.loaded_config_fugitive = 1
     local sta, _ = pcall(c, 'packadd vim-fugitive')
@@ -6,22 +11,10 @@ local fugitive_exe = function(cmd, refresh)
       print("no fugitive vim-fugitive")
       return
     end
-    sta, do_fugitive = pcall(require, 'do_fugitive')
-    if not sta then
-      print('no do_fugitive')
-      return
-    end
   end
-  if not do_fugitive then
-    return
-  end
-  do_fugitive.cmd("Gitsigns " .. cmd, refresh)
+  c(cmd)
 end
 
-
-local s = vim.keymap.set
-local a = vim.api
-local c = vim.cmd
 
 a.nvim_create_user_command('Fugitive', function(params)
   local fargs = params['fargs']
@@ -29,7 +22,7 @@ a.nvim_create_user_command('Fugitive', function(params)
   for i, v in ipairs(params['fargs']) do
     arg = arg .. ' ' .. v
   end
-  c(arg)
+  fugitive_exe(arg)
 end, { nargs = "*", })
 
 s({'n', 'v'}, '<leader>gg',  ":Fugitive Git<CR>", {silent = true})
