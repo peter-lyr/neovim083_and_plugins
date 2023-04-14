cfc78ae620f926d0682c51c405aa6a430b7f71cd
peter-lyr <llydrp.ldp@gmail.com>
2023-04-15 02:17:12 +0800

    whichkey g:whichkey_loaded

--- share/nvim/runtime/pack/my-packer/start/whichkey/plugin/whichkey.lua
+++ share/nvim/runtime/pack/my-packer/start/whichkey/plugin/whichkey.lua
@@ -1,7 +1,20 @@
-local sta, whichkey = pcall(require, "which-key")
-if not sta then
-  print('no which-key')
-  return
-end
+local g = vim.g
+local a = vim.api
+local c = vim.cmd
 
-whichkey.setup({})
+g.whichkey_lua = vim.fn['expand']('<sfile>')
+
+if not g.whichkey_loaded then
+  g.whichkey_loaded = 1
+  g.whichkey_cursormoved = a.nvim_create_autocmd({"CursorMoved"}, {
+    callback = function()
+      a.nvim_del_autocmd(g.whichkey_cursormoved)
+      local sta, whichkey = pcall(require, "which-key")
+      if not sta then
+        print("no which-key")
+        return
+      end
+      whichkey.setup({})
+    end,
+  })
+end
