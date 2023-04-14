1f53e580b415ed3266930f238f648ffc805dde28
peter-lyr <llydrp.ldp@gmail.com>
2023-04-14 11:39:05 +0800

    CopyPase.lua format code

--- share/nvim/runtime/pack/my-packer/opt/maps/plugin/CopyPase.lua
+++ share/nvim/runtime/pack/my-packer/opt/maps/plugin/CopyPase.lua
@@ -1,25 +1,25 @@
 local s = vim.keymap.set
 
-s({'n', 'v'}, '<a-y>', '"+y')
-s({'n', 'v'}, '<a-p>', '"+p')
-s({'n', 'v'}, '<a-s-p>', '"+P')
+s({ 'n', 'v' }, '<a-y>', '"+y')
+s({ 'n', 'v' }, '<a-p>', '"+p')
+s({ 'n', 'v' }, '<a-s-p>', '"+P')
 
 s({ 'c', 'i' }, '<a-w>', '<c-r>=g:word<cr>')
 s({ 'c', 'i' }, '<a-v>', '<c-r>"')
-s({ 't',     }, '<a-v>', '<c-\\><c-n>pi')
+s({ 't', }, '<a-v>', '<c-\\><c-n>pi')
 s({ 'c', 'i' }, '<a-=>', '<c-r>+')
-s({ 't',     }, '<a-=>', '<c-\\><c-n>"+pi')
+s({ 't', }, '<a-=>', '<c-\\><c-n>"+pi')
 s({ 'n', 'v' }, '<a-z>', '"zy')
 s({ 'c', 'i' }, '<a-z>', '<c-r>z')
-s({ 't',     }, '<a-z>', '<c-\\><c-n>"zpi')
+s({ 't', }, '<a-z>', '<c-\\><c-n>"zpi')
 
-s({'n', 'v'}, '<leader>y', '<esc>:let @+ = expand("%:t")<cr>', {silent = true})
-s({'n', 'v'}, '<leader>gy', '<esc>:let @+ = substitute(nvim_buf_get_name(0), "/", "\\\\", "g")<cr>', {silent = true})
+s({ 'n', 'v' }, '<leader>y', '<esc>:let @+ = expand("%:t")<cr>', { silent = true })
+s({ 'n', 'v' }, '<leader>gy', '<esc>:let @+ = substitute(nvim_buf_get_name(0), "/", "\\\\", "g")<cr>', { silent = true })
 
 local buf_leave = function()
   vim.g.word = vim.fn['expand']('<cword>')
 end
 
-vim.api.nvim_create_autocmd({"BufLeave"}, {
+vim.api.nvim_create_autocmd({ "BufLeave" }, {
   callback = buf_leave,
 })
