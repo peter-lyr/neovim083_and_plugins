245bf4daed0cd6337919b044b8e507b6f7c7fcb2
peter-lyr <llydrp.ldp@gmail.com>
2023-04-16 03:15:03 +0800

    leader leader h h bufferclean_all

--- share/nvim/runtime/pack/my-packer/start/bufferclean/lua/do_bufferclean.lua
+++ share/nvim/runtime/pack/my-packer/start/bufferclean/lua/do_bufferclean.lua
@@ -25,4 +25,45 @@ function M.do_bufferclean()
   end
 end
 
+function index_of(arr, val)
+  if not arr then
+    return nil
+  end
+  for i, v in ipairs(arr) do
+    if v == val then
+      return i
+    end
+  end
+  return nil
+end
+
+function rep(path)
+  local path, _ = string.gsub(path, '/', '\\')
+  return path
+end
+
+function M.do_bufferclean_all()
+  local cur_fname = rep(a['nvim_buf_get_name'](0))
+  local cur_wnr = f['bufwinnr'](f['bufnr']())
+  local ids = {}
+  local nms = {}
+  for wnr=1, f['winnr']('$') do
+    local bnr = f['winbufnr'](wnr)
+    if f['getbufvar'](bnr, '&buftype') ~= 'nofile' then
+      local fname = rep(a['nvim_buf_get_name'](bnr))
+      if wnr ~= cur_wnr then
+        if index_of(nms, fname) or cur_fname == fname then
+          table.insert(ids, f['win_getid'](wnr))
+        end
+      end
+      if not index_of(nms, fname) then
+        table.insert(nms, fname)
+      end
+    end
+  end
+  for k, v in ipairs(ids) do
+    a['nvim_win_hide'](v)
+  end
+end
+
 return M
--- share/nvim/runtime/pack/my-packer/start/bufferclean/plugin/bufferclean.lua
+++ share/nvim/runtime/pack/my-packer/start/bufferclean/plugin/bufferclean.lua
@@ -2,7 +2,7 @@ local s = vim.keymap.set
 local a = vim.api
 local g = vim.g
 
-local bufferclean_exe = function()
+local bufferclean_exe = function(cmd)
   if not g.loaded_do_bufferclean then
     g.loaded_do_bufferclean = 1
     sta, do_bufferclean = pcall(require, 'do_bufferclean')
@@ -14,11 +14,16 @@ local bufferclean_exe = function()
   if not do_bufferclean then
     return
   end
-  do_bufferclean.do_bufferclean()
+  if cmd == 'do_bufferclean' then
+    do_bufferclean.do_bufferclean()
+  else
+    do_bufferclean.do_bufferclean_all()
+  end
 end
 
 a.nvim_create_user_command('BufferClean', function(params)
-  bufferclean_exe()
+  bufferclean_exe(unpack(params['fargs']))
 end, { nargs = "*", })
 
-s({'n', 'v'}, '<leader>hh', ':BufferClean<cr>', {silent = true})
+s({'n', 'v'}, '<leader>hh', ':BufferClean do_bufferclean<cr>', {silent = true})
+s({'n', 'v'}, '<leader><leader>hh', ':BufferClean do_bufferclean_all<cr>', {silent = true})
